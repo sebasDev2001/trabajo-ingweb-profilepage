927da5bfd24aea56c64bab8b4f9eaff3e92745a8
Sebastian Sierra V <sebastian.sierrav2001@gmail.com>
2022-02-20 17:56:40 -0500

    Addded proyect pages and pictures in most of them

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,10 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+function copyText(){
+    var text = document.getElementById("myText").innerHTML;
+    var textCopy = document.getElementById("myText");
+    textCopy.value = text;    
+    document.execCommand('copy')
+}
